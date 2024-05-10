a28adf0d4528bccd9380cf9dbb70d75ac231f32a
wing <71299818+hwingu@users.noreply.github.com>
2024-05-10 14:27:38 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from '@vercel/analytics/react';
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
- 
-injectSpeedInsights();
+import { Analytics } from '@vercel/analytics/react'; 
 
 const inter = Inter({ subsets: ["latin"] });
 
